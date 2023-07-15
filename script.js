723de1ce790819eea4d05753853cfbcbf8093fd8
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-15 09:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function firstNonRepeatChar(str) {
+	const map = {};
+	for(let char of str) {
+		map[char] = (map[char] || 0) + 1;
+	}
+    for(let char of str) {
+        if(map[char] === 1) return char;
+    }
+    return 'a';
+}
